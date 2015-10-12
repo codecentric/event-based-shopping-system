709d0178f01a5dd9d2deff1f6acf0a961c5ae3ab
Marcel Birkner <marcel.birkner@gmail.com>
2015-10-12 17:28:23 +0200

    Create README.d

---
+++ orderEntryService/README.d
@@ -0,0 +1,11 @@
+## Running Order Entry Service
+
+### Prerequisite
+
+- Kafka and Zookeeper should be up and running, see https://github.com/codecentric/event-based-shopping-system/blob/master/README.md
+
+### Start Spring Boot App
+
+```
+mvn spring-boot:run
+```
